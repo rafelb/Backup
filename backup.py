6566f3309c72e0b7b94c63d460cba7306d6a6ec7
rafelb <rob.ellenbroek@gmail.com>
2019-11-28 13:13:05 +0100

    Add files via upload

--- backup.py
+++ backup.py
@@ -1,8 +1,7 @@
 ##
 # Script to compress files and put them in a separate location
-# with logging which files are backupped and whoch not
+# with logging which files are backupped and which not
 # created for windows OS
-# fixed backup location = 'V:\Rob\Backups\'
 # Rob Ellenbroek, Nov 2019, HYD
 # Written in Python 3.6
 # todo: combine tine / datatime stuff
@@ -13,7 +12,7 @@ import os
 import sys
 import zipfile
 import shutil
-from time import gmtime, strftime
+#from time import gmtime, strftime
 import logging
 import pandas as pd
 from datetime import datetime
@@ -60,18 +59,37 @@ if __name__ =='__main__':
     lckeuze = ''
     i = 1
     lc_dict = {}
-    dl = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    lc_date = strftime("%Y-%m-%d", gmtime())
+    ls_driveletters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    ln_source = 0
+
+    dateObj = datetime.now()
+    if dateObj.day < 10:
+        day = '0'+str(dateObj.day)
+    else:
+        day = str(dateObj.day)
+    if dateObj.month < 10:
+        month = '0'+str(dateObj.month)
+    else:
+        month = str(dateObj.month)
+    lc_date = str(dateObj.year) + '-' + month + '-' + day
+
+    print(lc_date)
     
     # dertermine drive kletters on system
-    drives = ['%s' % d for d in dl if os.path.exists('%s:' % d)]
+    drives = ['%s' % d for d in ls_driveletters if os.path.exists('%s:' % d)]
     
     while not((lckeuze.upper() in drives)):
         print('Van welke schijf wil je een backup maken (maak een keuze uit: ')
         lckeuze = input(drives)
 
+    # station where backup should be stpred cannot be the station, from which the backup originates
+    # hence this drive should be deleted from drive list
+    niet_backup_station = drives.index(lckeuze.upper())
+    drives.pop(niet_backup_station)
+
     # determine folders on chosen drive
     mappen = [dI for dI in os.listdir(lckeuze+':\\') if os.path.isdir(os.path.join(lckeuze+':\\',dI))]
+    aantal_mappen = len(mappen)
     
     # create a dictionary with folders
     for j in mappen:
@@ -81,15 +99,23 @@ if __name__ =='__main__':
     print('Mappen gevonden op '+lckeuze.upper()+':\\ ')
     for k, v in lc_dict.items():
         print('{:<8} {:<15}'.format(k, v))
+
     
     ln_source = input('Van welke map op '+lckeuze+':\\ wilt u een backup maken?')
 
     lc_folder = lc_dict[int(ln_source)]
 
     lc_source_folder = lckeuze.upper() + ':\\' + lc_folder + '\\'
-    lc_target_folder = 'V:\\Rob\\Backups\\'+lckeuze.upper()+'_'+lc_folder.upper()+'\\'
-    print(lc_source_folder)
-    print(lc_target_folder)
+
+    print('Naar welke schijf wilt u de backup wegschrijven:')
+    lc_target = input(drives)
+    
+    if lc_target.upper()=='V':
+        lc_target_folder = lc_target.upper()+':\\Rob\\Backups\\'+lckeuze.upper()+'_'+lc_folder.upper()+'\\'
+    else:
+        lc_target_folder = lc_target.upper()+':\\Backups\\'+lckeuze.upper()+'_'+lc_folder.upper()+'\\'
+        
+    print('Backup van : "' + lc_source_folder + '"...wordt gemaakt in: "' + lc_target_folder + '"')
 
     try:
         os.makedirs(lc_target_folder)
